e4c0b7009d70a7541e87992a234d225a96e1eb1e
Shazia Muhammad <Team2340@gmail.com>
2024-01-17 16:15:55 -0500

    We made the spark max work.

--- Xcentrics2024/src/main/java/frc/robot/RobotContainer.java
+++ Xcentrics2024/src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,14 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.*;
+
 
 public class RobotContainer {
     private static RobotContainer instance;
     private final Joystick joystick = new Joystick(Constants.DRIVER_JOYSTICK_PORT);
     private final Drivetrain drivetrain = new Drivetrain(joystick);
-    
+    private final EmilyTest emilyTest= new EmilyTest(joystick);
 
     public RobotContainer() {
         instance = this;
@@ -24,9 +25,12 @@ public class RobotContainer {
         new JoystickButton(joystick, 1)
         .onTrue( new InstantCommand( () -> {
             System.out.println("Joystick Button 1 Pressed");
+            emilyTest.startmotor();
+            
         }))
         .onFalse(new InstantCommand( () -> {
             System.out.println("Joystick Button 1 Released");
+            emilyTest.stopmotor();
         }));
 
 
