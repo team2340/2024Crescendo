01507d8eb55b3f583375439f8d43bf9a6bfa63ee
Shazia Muhammad <Team2340@gmail.com>
2024-01-17 17:39:44 -0500

    One you press button 2, if the camera sees target, the camera will measure distance and rotate to find target.

--- Xcentrics2024/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Xcentrics2024/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,9 +4,15 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Joystick;
 
 
@@ -15,6 +21,10 @@ public class Drivetrain extends SubsystemBase{
     private final WPI_TalonSRX leftRear = new WPI_TalonSRX(Constants.DRIVETRAIN_LEFT_REAR_MOTOR);
     private final WPI_TalonSRX rightFront = new WPI_TalonSRX(Constants.DRIVETRAIN_RIGHT_FRONT_MOTOR);
     private final WPI_TalonSRX rightRear = new WPI_TalonSRX(Constants.DRIVETRAIN_RIGHT_REAR_MOTOR);
+    private final PhotonCamera camera = new PhotonCamera("Microsoft_LifeCam_HD-3000");
+    private final PIDController controller = new PIDController(0.1,0,0.0);
+    private final PIDController turnController = new PIDController(0.1,0,0.0);
+    
 
     private final DifferentialDrive robotDrive;
     private final Joystick joystick;
@@ -47,7 +57,21 @@ public class Drivetrain extends SubsystemBase{
         factor= (factor*-1);
         forwardValue=joystick.getY() * factor;
         rotationValue=joystick.getX() * factor;
-       
+
+         var result = camera.getLatestResult();
+
+         PhotonTrackedTarget target = result.getBestTarget();
+        if(target != null && joystick.getRawButton(2)){
+        
+            double range =
+            PhotonUtils.calculateDistanceToTargetMeters(
+                0.2,
+                1,0,
+                Units.degreesToRadians(result.getBestTarget().getPitch()));
+            System.out.println(range);
+           // forwardValue= controller.calculate(range,-10.169);
+            rotationValue = -turnController.calculate(result.getBestTarget().getYaw(), 0);
+        }
 
         robotDrive.arcadeDrive(
             forwardBackwardLimiter.calculate(-forwardValue), 
